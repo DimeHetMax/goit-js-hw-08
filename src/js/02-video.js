c04de20bbc50e9ff86d3924f403446853354ba44
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-05-01 01:41:08 +0300

    second task
    Still remain to finish point 7

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', function({seconds}) {
+   localStorage.setItem("videoplayer-current-time", seconds)
+})
+player.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time")))
+
+ 
