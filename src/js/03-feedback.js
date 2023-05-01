d42a39d8f7b7018a6aecc66a4717f3f87c905ab0
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-05-01 18:33:24 +0300

    task 3
    homework is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+const throttled = throttle(onForm, 500)
+
+const form = document.querySelector(".feedback-form")
+form.addEventListener("input", throttled)
+form.addEventListener("submit", OnbtnSubmit)
+// console.dir(form.elements.email.value = "23")
+
+const feedbackForm = {
+    name: "",
+    message: "",
+}
+function onForm(event){
+    event.preventDefault() 
+    if(event.target.nodeName === "INPUT"){
+        feedbackForm.name = event.target.value;
+    } else if(event.target.nodeName === "TEXTAREA"){
+        feedbackForm.message = event.target.value;
+    }
+    localStorage.setItem("feedback-form-state",JSON.stringify(feedbackForm));
+}
+
+function OnbtnSubmit(event){
+    event.preventDefault()
+    const {name: userEmail, message: userMessage} = JSON.parse( localStorage.getItem("feedback-form-state"));
+    feedbackForm.name = userEmail;
+    feedbackForm.message = userMessage;
+    localStorage.removeItem("feedback-form-state");
+    console.log(feedbackForm)
+    event.currentTarget.reset()
+}
 
