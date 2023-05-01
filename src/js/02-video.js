88985fefcf4352d471d292bd864ad0f0267ab0fe
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-05-01 15:41:11 +0300

    task 2
    added throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,11 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const updateTimeThrottle = throttle(updateTime, 1000);
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-
-player.on('timeupdate', function({seconds}) {
-   localStorage.setItem("videoplayer-current-time", seconds)
-})
+player.on('timeupdate', updateTimeThrottle);
+function updateTime({seconds}){
+    localStorage.setItem("videoplayer-current-time", seconds);
+}
 player.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time")))
-
- 
